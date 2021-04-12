ec00c972ce02964fc4ce441352d6c321758490f3
masodo <41118685+masodo@users.noreply.github.com>
2021-04-12 16:26:27 -0400

    Installation parameters file

---
+++ WordSearch_masodo.php
@@ -0,0 +1,29 @@
+<?php
+/*--------------------------------------------------*/
+/* File Created by PracticalLightning Arcade 1.0!   */
+/* File Generated:     2021-04-12 00:00:0          */
+/*--------------------------------------------------*/
+/* original distribution file: ~Mike:D
+/*--------------------------------------------------*/
+
+$config = array(
+	'active'			=> '1',
+	'bgcolor'			=> '000000',
+	'gcat'			=> '5',
+	'gheight'			=> '700',
+	'gkeys'			=> '',
+	'gname'			=> 'WordSearch_masodo',
+	'gtitle'			=> 'WordSearch',
+	'gwidth'			=> '1024',
+	'gwords'			=> 'Hurry! Use the mouse to click a starting letter, then drag a line through the other
+letters tiles. They will highlight as you move across them. Release the mouse,
+and if it&rsquo;s a match the word will be struck off the list, and highlighted in
+the letter grid.',
+	'highscore_type'			=> 'low',
+	'object'			=> 'Hurry! Use the mouse to click a starting letter, then drag a line through the other
+letters tiles. They will highlight as you move across them. Release the mouse,
+and if it&rsquo;s a match the word will be struck off the list, and highlighted in
+the letter grid.',
+	'snggame'			=> '0',
+);
+?>
